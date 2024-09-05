a66ed268fc10eec25c96ec4721931436900f8e63
Tamás Gavrán <b0gavtam@vasvari.org>
2024-09-05 15:15:55 +0200

    deklaráció és definíció magyarázat

--- AdattipusokProject/Program.cs
+++ AdattipusokProject/Program.cs
@@ -3,5 +3,9 @@ Console.WriteLine("Hello, World!");
 
 int a = 1, b = 2;
 int x; // deklaráció
+//Console.WriteLine($"Deklaráció után: {x}.");
+//Hibaüzenet: Use of unassigned variable 'x'
 x = 10; // definíció
+Console.WriteLine($"Deklaráció után: {x}.");
 int y = 11; //deklaráció és definíció
+Console.WriteLine($"x = {y}");
