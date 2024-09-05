46742b12dacf07fb46ac13def7ac14ae3260555a
Tamás Gavrán <b0gavtam@vasvari.org>
2024-09-05 15:31:59 +0200

    unicode

--- AdattipusokProject/Program.cs
+++ AdattipusokProject/Program.cs
@@ -15,6 +15,6 @@ float flóath = 6 / 2.3F; flóath = (float)5 / 4; // csak 7 számjegyig képes p
 double dábül = 6 / 2.3; dábül = 6 / 2.3D; // 15-17 számjegyig képes pontosan számolni.
 
 byte bájt = 250; // Előjel nélküli 8 bites egész szám(0..255)
-char karakter = 'X'; // Egy Unicode karakter
+char karakter = 'X'; // Egy Unicode karakter(karakterkészlet, 1-4 bájton tárolja a karaktereket, UTF-8, 16, 32)
 decimal decimál = 6 / 2.3M; decimál = Convert.ToDecimal(6 / 2.3); // 25-29 számjegyig képes pontosan számolni. 
                                                                   // FONTOS: Pénz értéket mindig decimalban tároljunk!
