dfa5519b19d9623a1db4806f6851eb1dbecab5eb
Tamás Gavrán <b0gavtam@vasvari.org>
2024-09-05 15:27:04 +0200

    byte, char, decimal attadtípusok

--- AdattipusokProject/Program.cs
+++ AdattipusokProject/Program.cs
@@ -13,3 +13,8 @@ Console.WriteLine($"x = {y}");
 int integer = 0; // Előjeles, 32 bites egész szám (–2147483648.. 2147483647).
 float flóath = 6 / 2.3F; flóath = (float)5 / 4; // csak 7 számjegyig képes pontosan számolni.
 double dábül = 6 / 2.3; dábül = 6 / 2.3D; // 15-17 számjegyig képes pontosan számolni.
+
+byte bájt = 250; // Előjel nélküli 8 bites egész szám(0..255)
+char karakter = 'X'; // Egy Unicode karakter
+decimal decimál = 6 / 2.3M; decimál = Convert.ToDecimal(6 / 2.3); // 25-29 számjegyig képes pontosan számolni. 
+                                                                  // FONTOS: Pénz értéket mindig decimalban tároljunk!
