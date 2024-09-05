29584207fc6274c375fe92d28e46ae4094c9a81c
Tamás Gavrán <b0gavtam@vasvari.org>
2024-09-05 15:11:53 +0200

    deklaráció, definíció

---
+++ AdattipusokProject/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = 1, b = 2;
+int x; // deklaráció
+x = 10; // definíció
+int y = 11; //deklaráció és definíció
